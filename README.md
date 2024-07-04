3d91ddd8f237c905d9b207fb0690dd308de5c02f
Ekaitz <ekaitz@elenq.tech>
2024-07-04 13:00:08 +0200

    Database creation script

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # minisocial
 
-A mini social network for learning
+A mini social network for learning.
+
+
+### Create the database
+
+The `create_db.sql` file creates the sqlite3 database.
+Just run it.
---
+++ create_db.sql
@@ -0,0 +1,21 @@
+CREATE TABLE users (
+    user_id INTEGER PRIMARY KEY,
+    name TEXT,
+    last_name TEXT,
+    password TEXT
+);
+
+CREATE TABLE posts (
+    post_id INTEGER PRIMARY KEY,
+    body TEXT,
+    user_id INTEGER NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE follows (
+    followeR_id INTEGER,
+    followeD_id INTEGER,
+    PRIMARY KEY (followeR_id, followeD_id),
+    FOREIGN KEY (followeD_id) REFERENCES users(user_id),
+    FOREIGN KEY (followeR_id) REFERENCES users(user_id)
+);
