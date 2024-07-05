3c5385c7b8d04e0dbd07c94ee41963fb02ec487c
Ekaitz <ekaitz@elenq.tech>
2024-07-05 11:24:50 +0200

    Add some templates with nunjucks

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 import express from "express";
+import nunjucks from "nunjucks";
 
+let PORT = 3000;
 let app = express();
+nunjucks.configure('templates', {
+    express: app,
+    autoescape: true,   // Escape dangerous strings
+    watch: true         // Reload templates on change
+});
 
-app.get("/", (req, res)=>{
-    res.send("hola");
+app.get("/login", (req, res)=>{
+    res.render("login.njk");
 })
 
-app.listen(3000);
+app.listen(PORT, ()=>{
+    console.log("listening in port", PORT);
+});
