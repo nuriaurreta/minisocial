1b9f7c66c6bc97cdae187f4b0cdd51b5a378e5c0
Ekaitz <ekaitz@elenq.tech>
2024-07-05 11:37:41 +0200

    Prepare for the register forms

--- app.js
+++ app.js
@@ -8,10 +8,21 @@ nunjucks.configure('templates', {
     autoescape: true,   // Escape dangerous strings
     watch: true         // Reload templates on change
 });
+// For parsing application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }))
 
+// Routes
 app.get("/login", (req, res)=>{
     res.render("login.njk");
-})
+});
+
+app.get("/register", (req, res)=>{
+    res.render("register.njk");
+});
+app.post("/register", (req, res)=>{
+    console.log(req.body);
+    res.send("HAS MANDADO UN REGISTRO");
+});
 
 app.listen(PORT, ()=>{
     console.log("listening in port", PORT);
